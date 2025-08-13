7a9cf21b4de5d0846f0e7f2f4c443906986dfd77
Jawad Ali Yousafzai <137032884+JawadAliAI@users.noreply.github.com>
2025-08-13 08:35:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -107,4 +107,4 @@ async def root():
         return HTMLResponse(content="<h1>index.html not found</h1>", status_code=404)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
